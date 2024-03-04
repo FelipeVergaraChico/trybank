3f53ac98fbde6226d6963255992b7ab25ea9f6d1
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-03-04 00:21:08 -0300

    req 2...7

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -41,37 +41,101 @@ public class TrybankLib
     // 2. Construa a funcionalidade de fazer Login
     public void Login(int number, int agency, int pass)
     {
-        throw new NotImplementedException();
+        if (Logged)
+        {
+            throw new AccessViolationException("Usuário já está logado!");
+        }
+
+        for (int index = 0; index < registeredAccounts; index++)
+        {
+            if (Bank[index, 0] == number && Bank[index, 1] == agency)
+            {
+                if (Bank[index, 2] == pass)
+                {
+                    Logged = true;
+                    loggedUser = index;
+                }
+                else
+                {
+                    throw new ArgumentException("Senha incorreta!");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Agência + conta não encontrada");
+            }
+        }
     }
 
     // 3. Construa a funcionalidade de fazer Logout
     public void Logout()
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado!");
+        }
+
+        Logged = false;
+        loggedUser = -99;
     }
 
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();   
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado!");
+        }
+
+        return Bank[loggedUser, 3];
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
     public void Deposit(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado!");
+        }
+
+        Bank[loggedUser, 3] += value;
     }
 
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado!");
+        }
+
+        if (Bank[loggedUser, 3] < value)
+        {
+            throw new InvalidOperationException("Saldo insuficiente!");
+        }
+
+        Bank[loggedUser, 3] -= value;
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado!");
+        }
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[loggedUser, 3] < value)
+            {
+                throw new InvalidOperationException("Saldo insuficiente!");
+            }
+            else
+            {
+                Bank[loggedUser, 3] -= value;
+                Bank[i, 3] += value;
+            }
+        }
     }
 
    
